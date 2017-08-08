82a2b2adbdeff6d19a283a12e077b5e109513be9
mandeep <mandeep@localhost.localdomain>
2017-08-08 02:32:27 -0400

    roboto

---
+++ include/robot/roboto/RobotoProtocol.hpp
@@ -0,0 +1,22 @@
+/*
+  Project: OpenCogER
+  File: RobotoProtocol.hpp
+  Author: Mandeep Singh Bhatia
+  License: AGPL
+  Date: 2017
+*/
+//roboto control class
+#ifndef ROBOTOPROTOCOL_H
+#define ROBOTOPROTOCOL_H
+
+
+//convert sensory data to processing format before using
+//e.g. replace CamCapture with RCamCapture, maybe provide even flag in imagesource for new image
+//functions for action
+class RobotoProtocol
+{
+    public:
+    
+};
+
+#endif
