0bc17a2e75edf148a2bd07539a35130cb24ab981
mandeep <mandeep.singh.bhatia@gmail.com>
2017-08-15 17:59:47 +0530

    string udp server

--- include/network/StringServerUDP.hpp
+++ include/network/StringServerUDP.hpp
@@ -7,7 +7,54 @@
 */
 #ifndef STRINGSERVERUDP_H
 #define STRINGSERVERUDP_H
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include <string>
+#include <map>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <thread>
+#include <mutex>
+
+using namespace std;
+
 class StringServerUDP
 {
+public:
+    StringServerUDP(vector<string>tag_names,int portno);
+    ~StringServerUDP();
+    bool isOk();
+    string readCurrentString(string tag);
+
+protected:
+    bool ok;
+    map<string,string> msg_map;
+    vector<string> tags;
+    thread* th;
+    mutex mtx;
+    
+    int sockfd; /* socket */
+    struct sockaddr_in serveraddr; /* server's addr */
+    struct sockaddr_in clientaddr; /* client addr */
+    socklen_t clientlen; /* byte size of client's address */
+    struct hostent *hostp; /* client host info */
+
+    //char *hostaddrp; /* dotted decimal host addr string */
+    //int optval; /* flag value for setsockopt */
+    //int n; /* message byte size */
+
+private:
+    bool addTag(string tag);
+    static void threadLoop(StringServerUDP* ss);
 };
 #endif
---
+++ network/StringServerUDP.cpp
@@ -0,0 +1,133 @@
+/*
+  Project: OpenCogER
+  File: StringServerUDP.cpp
+  Author: Mandeep Singh Bhatia
+  License: AGPL
+  Date: 2017
+*/
+#include "network/StringServerUDP.hpp"
+#define BUFSIZE 1024
+
+StringServerUDP::StringServerUDP(vector<string>tag_names,int portno)
+{
+  //portno; /* port to listen on */
+  
+  ok=false;
+  for (int i=0;i<tag_names.size();i++)
+  {
+      addTag(tag_names[i]);
+  }
+  if (tags.size()<1)return;
+  /* 
+  * socket: create the parent socket 
+  */
+  sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  if (sockfd < 0){
+      cout<<"\nError opening socket"<<endl;
+      return;
+  } 
+   /* setsockopt: Handy debugging trick that lets 
+   * us rerun the server immediately after we kill it; 
+   * otherwise we have to wait about 20 secs. 
+   * Eliminates "ERROR on binding: Address already in use" error. 
+   */
+  int optval = 1;
+  setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, 
+	     (const void *)&optval , sizeof(int));
+
+  /*
+   * build the server's Internet address
+   */
+  bzero((char *) &serveraddr, sizeof(serveraddr));
+  serveraddr.sin_family = AF_INET;
+  serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  serveraddr.sin_port = htons((unsigned short)portno);
+
+  /* 
+   * bind: associate the parent socket with a port 
+   */
+  if (bind(sockfd, (struct sockaddr *) &serveraddr, 
+	   sizeof(serveraddr)) < 0){ 
+    cout<<"\nERROR on binding"<<endl;
+    return;
+  }
+  clientlen = sizeof(clientaddr);
+
+  ok=true;
+  th = new thread(threadLoop,this);
+}
+
+StringServerUDP::~StringServerUDP()
+{
+    ok=false;
+    if (th){
+        th->join();
+        delete th;
+    }
+}
+
+bool StringServerUDP::isOk()
+{
+    return ok;
+}
+
+bool StringServerUDP::addTag(string tag)
+{
+    if (tag.length()<1) return false;
+    if (tags.size()<1) {
+        tags.push_back(tag);
+        return true;
+    }
+    if (find(tags.begin(),tags.end(),tag)!=tags.end()){
+        return false;
+    }
+    tags.push_back(tag);
+    return true;
+}
+
+string StringServerUDP::readCurrentString(string tag)
+{
+    string result;
+    if (find(tags.begin(),tags.end(),tag)==tags.end())
+        return result;
+    mtx.lock();
+    result = msg_map[tag];
+    msg_map[tag] = "";
+    mtx.unlock();
+    return result;
+}
+
+void StringServerUDP::threadLoop(StringServerUDP* ss)
+{
+    char buf[BUFSIZE]; /* message buf */
+    string tmp,tg,st;
+    size_t pos;
+    int n;
+    while (ss->ok){
+      /*
+       * recvfrom: receive a UDP datagram from a client
+      */
+      bzero(buf, BUFSIZE);
+      n = recvfrom(ss->sockfd, buf, BUFSIZE, 0,
+		 (struct sockaddr *) &(ss->clientaddr), &(ss->clientlen));
+      if (n < 0)
+        cout<<"ERROR in recvfrom"<<endl;
+      if (n >2 && n < BUFSIZE){
+          if (buf[n-1]!='\0')buf[n]='\0';
+          //process buf
+          if (buf[0]!='<') continue;
+          tmp=buf;
+          pos=tmp.find_first_of('>',2);
+          if (pos==string::npos)continue;
+          //look for tag
+          tg = tmp.substr(0,pos-1);
+          if (find(ss->tags.begin(),ss->tags.end(),tg)==ss->tags.end())continue;
+          if (pos<tmp.length()-1){
+              st = tmp.substr(pos,tmp.length()-pos-1);
+              ss->mtx.lock();
+              ss->msg_map[tg]=st;
+              ss->mtx.unlock();
+          }
+      }
+    }
+}
