a47c726592429d7b93a3f342724106bc28cb387c
Dagim Sisay <dagiopia@gmail.com>
2018-09-05 18:05:49 +0300

    Update and rename FindTinyCog.cmake to TinyCogConfig.cmake

--- cmake/TinyCogConfig.cmake
+++ cmake/TinyCogConfig.cmake
@@ -15,15 +15,8 @@ FIND_LIBRARY(DR_ROBOTO NAMES dr_roboto HINTS /usr/lib/TinyCog /usr/local/lib/Tin
 
 set(TINYCOG_LIBRARIES ${SENSOR_VISION} ${SENSOR_AUDIO} ${ACT_AUDIO} ${NETWORK} ${SPI_COMM} ${DR_ROBOTO})
 
-find_path(VISION_INCLUDE_DIR1 BoxTracker.hpp
+find_path(VISION_INCLUDE_DIR VisionCommon.hpp
 		  HINTS /usr/include/TinyCog/sense/vision /usr/local/include/TinyCog/sense/vision)
-
-find_path(VISION_INCLUDE_DIR2 RaspiVision.hpp
-		  HINTS /usr/include/TinyCog/sense/vision /usr/local/include/TinyCog/sense/vision)
-
-find_path(VISION_INCLUDE_DIR3 Vision.hpp
-		  HINTS /usr/include/TinyCog/sense/vision /usr/local/include/TinyCog/sense/vision)
-
 
 find_path(TTS_INCLUDE_DIR FestivalTTS.hpp
 		  HINTS /usr/include/TinyCog/act/audio /usr/local/include/TinyCog/act/audio)
@@ -36,14 +29,9 @@ find_path(COMM_INCLUDE_DIR SpiComm.hpp
 		  HINTS /usr/include/TinyCog/comm/spi /usr/local/include/TinyCog/comm/spi)
 
 
-set(TINYCOG_INCLUDE_DIR ${VISION_INCLUDE_DIR1} ${VISION_INCLUDE_DIR2} ${VISION_INCLUDE_DIR3} ${TTS_INCLUDE_DIR} ${NETWORKR_INCLUDE_DIR} ${COMM_INCLUDE_DIR})
+set(TINYCOG_INCLUDE_DIR ${VISION_INCLUDE_DIR} ${TTS_INCLUDE_DIR} ${NETWORKR_INCLUDE_DIR} ${COMM_INCLUDE_DIR})
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(TinyCog DEFAULT_MSG SENSOR_VISION SENSOR_AUDIO ACT_AUDIO NETWORK SPI_COMM DR_ROBOTO VISION_INCLUDE_DIR1 VISION_INCLUDE_DIR2 VISION_INCLUDE_DIR3 TTS_INCLUDE_DIR NETWORKR_INCLUDE_DIR COMM_INCLUDE_DIR)
 
 mark_as_advanced(TINYCOG_INCLUDE_DIR TINYCOG_LIBRARIES)
-
-
-
-
-
