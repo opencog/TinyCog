8132c7da24060d6ceeb126a08c20e84fba831513
Dagim <dagiopia@gmail.com>
2018-11-16 12:30:34 +0000

    emopy emotion recognizer grpc client

--- comm/rpc/RPC_Client.cpp
+++ comm/rpc/RPC_Client.cpp
@@ -17,69 +17,42 @@ void RPC_Client::encode_img(cv::Mat in)
 }
 
 
-bool RPC_Client::detect_faces(cv::Mat &in, std::vector<cv::Rect> &out)
+std::string RPC_Client::encode_img_b64(cv::Mat in)
 {
+    vbuff.clear();
+    cv::imencode(IMG_ENCODING, in, vbuff);
+    unsigned char *enc_msg = new unsigned char[vbuff.size()];
+    for(int i=0; i < vbuff.size(); i++) enc_msg[i] = vbuff[i];
+    std::string encoded = base64_encode(enc_msg, vbuff.size());
+    return encoded;
+}
+
+
+bool RPC_Client::detect_emotion(cv::Mat &in, std::vector<std::string> &out_emos, 
+                                             std::vector<cv::Rect> &out_boxes)
+{
+    Status status;
     ClientContext ctxt;
-    ImageBase::Image img;
-    ImageBase::Faces faces;
-    encode_img(in);
-    img.set_data(ucbuff, vbuff.size());
-    img.set_operation(0); //unused for now
-    status = stub_->DetectFaces(&ctxt, img, &faces);
-    if(!status.ok())
+    RecognizeRequest img;
+    RecognizeResponse resp;
+
+    out_emos.clear();
+    out_boxes.clear();
+    img.set_image(encode_img_b64(in).c_str());
+    img.set_image_type("jpg");
+    status = stub_->classify(&ctxt, img, &resp);
+    if(!status.ok()) {
+        printf("Error: %s\n", status.error_message().c_str());
         return false;
-    for(size_t idx = 0; idx < faces.faces_size(); idx++) {
-        rct.x = faces.faces(idx).x();
-        rct.y = faces.faces(idx).y();
-        rct.width = faces.faces(idx).w();
-        rct.height = faces.faces(idx).h();
-	out.push_back(rct);
+    }
+    for(size_t idx = 0; idx < resp.predictions_size(); idx++) {
+        out_emos.push_back(resp.predictions(idx));
+	out_boxes.push_back(cv::Rect(resp.bounding_boxes(idx).x(),
+	                          resp.bounding_boxes(idx).y(),
+			          resp.bounding_boxes(idx).w(),
+			          resp.bounding_boxes(idx).h()));
     }
     return true;
 }
 
 
-//the output param is only a vvp because I don't know how to insert into full_object_detection obj
-bool RPC_Client::detect_face_lms(cv::Mat &in, std::vector<std::vector<cv::Point> > &out)
-{
-    ClientContext ctxt;
-    ImageBase::Image img;
-    ImageBase::Landmarks lms;
-    encode_img(in);
-    img.set_data(ucbuff, vbuff.size());
-    img.set_operation(0); //unused for now
-    status = stub_->FaceLandmarks(&ctxt, img, &lms);
-    if(!status.ok())
-        return false;
-
-    cv::Point pt;
-    for(size_t idx = 0; idx < lms.landmarks_size(); idx++) { //on faces
-        facial_lms shape; 
-	std::vector<cv::Point> flm;
-	for (size_t jdx = 0; jdx < lms.landmarks(idx).points_size(); jdx++) {
-	    pt.x = lms.landmarks(idx).points(jdx).x();
-	    pt.y = lms.landmarks(idx).points(jdx).y();
-	    flm.push_back(pt);
-	}
-	out.push_back(flm);
-    }
-    
-    return true;
-}
-
-
-bool RPC_Client::salient_point(cv::Mat &in, cv::Point &out)
-{
-    ClientContext ctxt;
-    ImageBase::Image img;
-    ImageBase::Point pt;
-    encode_img(in);
-    img.set_data(ucbuff, vbuff.size());
-    img.set_operation(0); //unused for now
-    status = stub_->SalientPoint(&ctxt, img, &pt);
-    if(!status.ok())
-        return false;
-    out.x = pt.x();
-    out.y = pt.y();
-    return true;
-}
--- examples/comm/rpc/RPC_Client_Test.cpp
+++ examples/comm/rpc/RPC_Client_Test.cpp
@@ -7,34 +7,38 @@
 
 #include "comm/rpc/RPC_Client.hpp"
 #include "sense/vision/RaspiVision.hpp"
-
 #include <opencv2/opencv.hpp>
+#include <iostream>
 #include <vector>
 #include <unistd.h>
 
+
+
 int main(int argc, char **argv)
 {
-    RaspiCamCapture *rcap = RaspiCamCapture::init("rcap", 320, 240, 20);
-    RPC_Client rpcc;
-    cv::Mat frame; 
-    std::vector<cv::Rect> faces;
-    std::vector<std::vector<cv::Point> > f_lms;
-    cv::Point spt;
-    frame = rcap->getCurrentFrame();
-    std::cout<<"Read Frame\n";
-    bool fd = rpcc.detect_faces(frame, faces);
-    bool fl = rpcc.detect_face_lms(frame, f_lms);
-    bool sp = rpcc.salient_point(frame, spt);
-    printf("Done!\n");
-    if (fd) {
-        for(int i = 0; i < faces.size(); i++) {
-            cv::rectangle(frame, faces[i], cv::Scalar(0, 255, 0), 2, 1);
-	    for (int j = 0; j < f_lms[i].size(); j++)
-	        cv::circle(frame, f_lms[i][j], 1.5, cv::Scalar(0, 255, 255), 1.5);
+	RaspiCamCapture *rcap = RaspiCamCapture::init("rcap", 320, 240, 20);
+	ITColor2Gray c2g("c2g");
+	
+	RPC_Client rpcc;
+
+	cv::Mat frame;
+	while(true) {
+		frame = rcap->getCurrentFrame();
+		//std::cout<<"Read Frame\n";
+		std::vector<std::string> emo_preds;
+		std::vector<cv::Rect> emo_boxes;
+		bool ed = rpcc.detect_emotion(frame, emo_preds, emo_boxes);
+		printf("Done! %d \n", (int)ed);
+		if (ed) {
+	    	for(uint32_t idx = 0; idx < emo_preds.size(); idx++) {
+				cv::rectangle(frame, emo_boxes[idx], cv::Scalar(0, 255, 0), 2, 1);
+				cv::putText(frame, emo_preds[idx], cv::Point(emo_boxes[idx].x, emo_boxes[idx].y),
+				            FONT_HERSHEY_COMPLEX_SMALL, 2, cv::Scalar(0, 255, 0), 1, CV_AA);
+			}
+		}
+		cv::imshow("FaceRPC", frame);
+		if(27 == cv::waitKey(10)) break;
+		usleep(2000); //break;
 	}
-	cv::circle(frame, spt, 2, cv::Scalar(0, 0, 255), 3);
-        cv::imshow("FaceRPC", frame);
-        cv::waitKey(0);
-    }
-return 0;
+	return 0;
 }
