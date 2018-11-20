b340f3fa83484caa37a0430d36523ab35693c542
Dagim <dagiopia@gmail.com>
2018-11-20 13:15:01 +0000

    add emotion recognition grpc service from https://github.com/tesYolan/emotion-recognition-service

---
+++ comm/rpc/RPC_Client_Face_Emotion.cpp
@@ -0,0 +1,50 @@
+/*
+  File: RPC_Client_Face_Emotion.cpp
+  Date: November, 2018
+  Author: Dagim Sisay <dagiopia@gmail.com>
+  License: AGPL
+*/
+
+
+#include "comm/rpc/RPC_Client_Face_Emotion.hpp"
+
+
+void RPC_Client_Face_Basic::encode_img(cv::Mat in)
+{
+	vbuff.clear();
+	cv::imencode(IMG_ENCODING, in, vbuff);
+	ucbuff = &vbuff[0];
+}
+
+
+bool RPC_Client_Face_Basic::detect_faces(cv::Mat &in, std::vector<std::string> &out_emos,
+																		std::vector<cv::Rect> &out_boxes)
+{
+	ClientContext ctxt;
+	RecognizeRequest img;
+	RecognizeResponse resp;
+	 
+
+	out_emos.clear();
+	out_boxes.clear();
+	img.set_image(encode_img(in));
+	img.set_image_type(IMG_ENCODING);
+	status = stub_->classify(&ctxt, img, &resp);
+	 
+	if(!status.ok()) { 
+		fprintf(stderr, "Error: %s\n", status.error_message().c_str()); 
+		return false; 
+	}
+
+	for(size_t idx = 0; idx < resp.predictions_size(); idx++) {
+		printf("Emo: %s\n", resp.predictions(idx).c_str()); //XXX Remove
+		out_emos.push_back(resp.predictions(idx));
+		out_boxes.push_back(cv::Rect(resp.bounding_boxes(idx).x(),
+		                             resp.bounding_boxes(idx).y(),
+		                             resp.bounding_boxes(idx).w(),
+		                             resp.bounding_boxes(idx).h()));
+	}
+	return true;
+}
+
+
