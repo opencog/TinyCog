f54a074f0df3a062c8bf00fe1ba447e4c41a1e59
Amanopia <amankassahun190@gmail.com>
2019-02-20 12:47:40 +0000

    manual commands for enestien robot

---
+++ examples/act/manual/exampleManualCommands.cpp
@@ -0,0 +1,51 @@
+/*
+  Project: OpenCogER
+  File: exampleManualCommands.cpp
+  Author: Aman Kassahun
+  License: AGPL
+  Date: February, 2019
+*/
+
+#include "act/manual/manualCommands.cpp"
+
+ int main(int argc, char *argv[]) { 
+
+
+if(wiringPiSetupPhys()==-1){ cout<<"Setup wiring pi failed"; 
+return 1;
+}
+
+/*the follwing some common facial expressions
+  Winking,yawning and laughing.
+*/
+
+resetAll();
+//Winking
+moveBodyAbsolute(forehead,0,100);
+moveBodyAbsolute(forehead,20,100);
+moveBodyAbsolute(check,20,100);
+moveBodyAbsolute(check,0,100);
+
+
+//Yawning
+moveBodyAbsolute(mouth,45,100);
+sleep(3);
+moveBodyRelative(mouth,-5,100);//moveBodyAbsolute(mouth,43,100);
+moveBodyAbsolute(mouth,45,100);
+for( int i=1;i<=5;i++)
+moveBodyRelative(mouth,-5,5);
+
+//laughing
+for(int i=0;i<4;i++)
+{
+for(int i=0;i<=16;i++)
+{
+moveBodyAbsolute(mouth,45,100);
+moveBodyRelative(mouth,-5,100);
+
+}
+moveBodyAbsolute(mouth,35,20);
+}
+
+}
+
